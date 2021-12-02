aed7f1e74e04bd37811830c49fd7394504a49326
goldenyak <goldenyakegor@gmail.com>
2021-12-02 21:55:31 +0300

    add some functions

--- my-app/src/components/UserList.tsx
+++ my-app/src/components/UserList.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import {useSelector} from "react-redux";
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
 import {useTypesSelector} from "../hooks/useTypesSelector";
+import {fetchUsers} from "../store/actionCreators/user";
 
 const UserList: React.FC = () => {
 
     const {users, loading, error} = useTypesSelector(state => state.user) // С помощью типизированного кастомного useSelector вынимаем состояние нашего приложения в любом компоненте
-    console.log(users);
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
+
+    if (loading === true) {
+        return <h1>Идет загрузка пользователей...</h1>
+    }
+    if (error) {
+        return <h1>{error}</h1>
+    }
+
     return (
         <div>
-            
+            {users.map(user =>
+            <div>{user.name}</div>)}
         </div>
     );
 };
