63e4cadabdfe62dea3870088388e1cd26ef1741d
goldenyak <goldenyakegor@gmail.com>
2021-12-02 23:27:21 +0300

    add some functions

--- my-app/src/components/TodoList.tsx
+++ my-app/src/components/TodoList.tsx
@@ -1,18 +1,19 @@
 import React, {useEffect} from 'react';
 import {useTypesSelector} from "../hooks/useTypesSelector";
 import {useDispatch} from "react-redux";
-import {fetchUsers} from "../store/actionCreators/user";
-import {fetchTodos} from "../store/actionCreators/todos";
+import {fetchTodos, setTodoPage} from "../store/actionCreators/todos";
 
 const TodoList: React.FC = () => {
 
     const {todos, loading, page, limit, error} = useTypesSelector(state => state.todo)
+    const pages = [1, 2, 3, 4, 5]
 
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(fetchTodos(page, limit))
-    }, [])
+    }, [page])
+
 
     if (loading === true) {
         return <h1>Идет загрузка списка дел...</h1>
@@ -24,7 +25,16 @@ const TodoList: React.FC = () => {
     return (
         <div>
             {todos.map(todo =>
-            <div key={todo.id}>{todo.id} - {todo.title}</div>)}
+                <div key={todo.id}>{todo.id} - {todo.title}</div>)}
+            <div style={{display: 'flex'}}>
+                {pages.map(p =>
+                    <div onClick={() => dispatch(setTodoPage(p))}
+                         style={{
+                             padding: '10px',
+                             border: p === page ? '2px solid green' : '1px solid grey'
+                         }}>{p}</div>)}
+            </div>
+
         </div>
     );
 };
